abcd316507bc39c8b25a6e064931a6f908a2b080
Loki2140 <93878720+Loki2140@users.noreply.github.com>
2022-03-28 01:53:43 +0300

    Lesson#2

---
+++ JS/dz2.js
@@ -0,0 +1,83 @@
+// Calculator - усложнял исключительно в образовательных целях.
+
+export default function dz2() {
+  debugger;
+  const firstNumber = Number(getNumber());
+  const secondNumber = Number(getNumber());
+  const symbol = String(getSymbol());
+  console.log(symbol);
+  let total;
+
+  if (isNaN(firstNumber) || isNaN(secondNumber) || symbol === "undefined") {
+    alert("Вы не ввели один из важных параметров! Задача завершена!");
+    return;
+  }
+
+  function getNumber() {
+    const number = prompt("Введите число!");
+    if (number === null) {
+      alert("Вы отменили выполнение задачи!");
+    } else if (isNaN(number) || number === "") {
+      alert("Это не число Введите число!");
+      return getNumber();
+    } else {
+      return number;
+    }
+  }
+
+  function getSymbol() {
+    const symbol = prompt(
+      "Введите один из доступных символов! '/' | '*' | '+' | '-'"
+    );
+    if (symbol === null) {
+      alert("Вы отменили выполнение задачи!");
+    } else if (["/", "+", "*", "-"].includes(symbol)) {
+      return symbol;
+    } else {
+      alert("Это не один из символов!");
+      return getSymbol();
+    }
+  }
+
+  function finalAlert(firstNumber, secondNumber, symbol, total) {
+    alert(`${firstNumber}${symbol}${secondNumber}=${total}`);
+  }
+
+  switch (symbol) {
+    case "+":
+      finalAlert(
+        firstNumber,
+        secondNumber,
+        symbol,
+        (total = firstNumber + secondNumber)
+      );
+      break;
+    case "-":
+      finalAlert(
+        firstNumber,
+        secondNumber,
+        symbol,
+        (total = firstNumber - secondNumber)
+      );
+      break;
+    case "/":
+      finalAlert(
+        firstNumber,
+        secondNumber,
+        symbol,
+        (total = firstNumber / secondNumber)
+      );
+      break;
+    case "*":
+      finalAlert(
+        firstNumber,
+        secondNumber,
+        symbol,
+        (total = firstNumber * secondNumber)
+      );
+      break;
+    default:
+      alert("Что-то пошло не так...");
+      break;
+  }
+}
--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
 import dz1 from "../JS/dz1.js";
+import dz2 from "../JS/dz2.js";
 
-let el = document.getElementById("dz1Button").addEventListener("click", dz1);
+let dz1Button = document
+  .getElementById("dz1Button")
+  .addEventListener("click", dz1);
+let dz2Button = document
+  .getElementById("dz2Button")
+  .addEventListener("click", dz2);
+// let dz2_1Button = document
+//   .getElementById("dz2_1Button")
+//   .addEventListener("click", dz2_1);
