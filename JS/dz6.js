b1dedd98245ec7f819586350f95a25ef29e7c082
Loki2140 <93878720+Loki2140@users.noreply.github.com>
2022-04-11 13:59:02 +0300

    Lesson#6

---
+++ JS/dz6.js
@@ -0,0 +1,37 @@
+export default function dz6() {
+  const obj = {
+    name: "Alex",
+    age: 33,
+    stage: null,
+    adress: {
+      country: "UA",
+      city: "Dnipro"
+    },
+    marks: { history: [2, 3, 4, [2, 3, 4, 5]], math: [[2, 3, 4, 5], 5, 3, 5] }
+  };
+
+  const objCopy = copy(obj);
+
+  console.log("Original obj ", obj);
+  console.log("Original obj is changing!!!");
+  obj.adress.country = "POL";
+  obj.adress.city = "Warsaw";
+  obj.name = "Tom";
+  obj.marks.history.shift();
+  obj.marks.math.pop();
+  obj.marks.math[0].pop();
+  console.log("Original obj is changed!!!");
+  console.log("Changed obj ", obj);
+  console.log("Copy obj ", objCopy);
+
+  function copy(obj) {
+    let newObj = Array.isArray(obj) ? [] : {};
+    for (const val in obj) {
+      newObj[val] =
+        typeof obj[val] === "object" && obj[val] !== null
+          ? copy(obj[val])
+          : obj[val];
+    }
+    return newObj;
+  }
+}
