eb48f37413f793b7744b36f2d90699deda07eb82
Loki2140 <93878720+Loki2140@users.noreply.github.com>
2022-05-24 18:09:31 +0300

    Lesson#17

---
+++ dz17/RestApi.js
@@ -0,0 +1,26 @@
+class RestApi {
+  _sendRequest(url, method = "GET", body = null) {
+    return fetch(url, {
+      method: method,
+      body: body != null ? JSON.stringify(body) : null,
+      headers: { "Content-Type": "application/json" }
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response.json().then((error) => {
+        const err = new Error("Что-то пошло не так!");
+        err.date = error;
+        throw err;
+      });
+    });
+  }
+
+  async loadOneItem(url, id = "1") {
+    return await this._sendRequest(url + id);
+  }
+
+  loadList(url) {
+    return this._sendRequest(url);
+  }
+}
---
+++ dz17/dz17.js
@@ -0,0 +1,88 @@
+"use strict";
+const TABLE_ITEM_TEMPL = document.getElementById("textTableTemplate").innerHTML;
+const BLOCK_IMG_TEMPL = document.getElementById("imgBlockItem").innerHTML;
+const IMG_ITEM_TEMPL = document.getElementById("imgBigItem").innerHTML;
+imgBigItem;
+const URL_REQUEST_ALBUMS = "https://jsonplaceholder.typicode.com/albums/";
+const URL_REQUEST_ALBUM =
+  "https://jsonplaceholder.typicode.com/photos?albumId=";
+const HEADERS = { "Content-Type": "application/json" };
+const ALBUM_ITEM_CLASS = "trItem";
+const IMG_ITEM_CLASS = "flexImg";
+const BIG_IMG_ITEM_CLASS = "bigImg";
+const ALBUM_ITEM_ATTR = "data-tr-id";
+const IMG_ITEM_ATTR = "data-img-id";
+
+const albumsBlock = document.querySelector(".tbody");
+const imgBlock = document.querySelector(".imgBlock");
+
+let albumList = [];
+let imgList = [];
+const albumListApi = new RestApi();
+
+albumsBlock.addEventListener("click", onAlbumsClick);
+imgBlock.addEventListener("click", onImgClick);
+
+init();
+function init() {
+  albumListApi
+    .loadList(URL_REQUEST_ALBUMS)
+    .then((data) => {
+      albumList = data;
+      renderItemList(albumsBlock, TABLE_ITEM_TEMPL, albumList);
+    })
+    .catch((err) => console.log(err));
+
+  albumListApi
+    .loadOneItem(URL_REQUEST_ALBUM)
+    .then((data) => {
+      imgList = data;
+      renderItemList(imgBlock, BLOCK_IMG_TEMPL, imgList);
+    })
+    .catch((err) => console.log(err));
+}
+function renderItemList(block, template, list) {
+  block.innerHTML = list.map((el) => interpolate(template, el)).join("\n");
+}
+function interpolate(template, obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      template = interpolate(template, obj[key]);
+    }
+    template = template.replaceAll(`{{${key}}}`, obj[key]);
+  }
+  return template;
+}
+
+function onAlbumsClick(event) {
+  const target = event.target;
+  albumListApi
+    .loadOneItem(
+      URL_REQUEST_ALBUM,
+      getItemId(target, ALBUM_ITEM_CLASS, ALBUM_ITEM_ATTR)
+    )
+    .then((data) => {
+      imgList = data;
+      renderItemList(imgBlock, BLOCK_IMG_TEMPL, imgList);
+    })
+    .catch((err) => console.log(err));
+}
+function getItemId(el, ItemClass, id) {
+  const tableItemId = el.closest("." + ItemClass);
+  return +tableItemId.getAttribute(id);
+}
+
+function onImgClick(event) {
+  const target = event.target;
+  const id = getItemId(target, IMG_ITEM_CLASS, IMG_ITEM_ATTR);
+  const item = [imgList.find((el) => el.id === id)];
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    interpolate(IMG_ITEM_TEMPL, item)
+  );
+  const bigBlock = document.querySelector(".bigImg");
+  bigBlock.addEventListener("click", delBigImg);
+}
+function delBigImg() {
+  document.querySelector(".bigImg").remove();
+}
